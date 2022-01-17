78ffe14f4e8a7ccad28e1e9d413c741aa79ae4ec
Emmrys-Jay <jonathanemma121@gmail.com>
2022-01-17 16:53:21 +0100

    completed exercise 3

---
+++ ex3.go
@@ -0,0 +1,21 @@
+/*
+
+Create a new type called person which is STRUCT that stores fName and lName
+
+*/
+
+package main
+
+type person struct {
+	fname string
+	lname string
+}
+
+func main() {
+	/*p1 := person{
+		"Ade",
+		"Tokunbo",
+	}
+
+	fmt.Println(p1)*/
+}

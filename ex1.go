b3352fbbd14c85a2e782ae2867fabad24ac7538d
Emmrys-Jay <jonathanemma121@gmail.com>
2022-01-17 16:36:16 +0100

    completed ex1

--- ex1.go
+++ ex1.go
@@ -9,6 +9,20 @@ range over the slice printing out both the index and the value
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+	fmt.Println(mySlice)
+
+	for i, _ := range mySlice {
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Printf("\n")
+
+	for i, val := range mySlice {
+		fmt.Printf("%d: %d\n", i, val)
+	}
 }
